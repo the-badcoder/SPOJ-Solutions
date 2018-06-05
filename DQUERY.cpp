03206163ffef9dbb00b5f04efeb5a62cecdc399d
Tareq Aziz Khan <xktareq07@gmail.com>
2018-06-05 11:02:14 +0600

    DQUERY Code Updated.
    MO + Segement Tree ( Offline )

--- DQUERY.cpp
+++ DQUERY.cpp
@@ -1,106 +1,158 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-using ll = long long;
-using ii = pair < int, int >;
-const int res = 2e6 + 10;
-const int res1 = 1e6 + 10;
 
-int n, q, BLOCK_SIZE, a[res];
-ll current_answer, digit[res], answers[res];
+#define READ                   freopen("input.txt", "r", stdin);
+#define WRITE                  freopen("output.txt", "w", stdout);
+#define space                  " "
+#define clr(ar)                memset( ar, 0, sizeof ar )
 
-pair < ii, int > queries[res];
+#define sf1(a)                scanf("%d", &a)
+#define sf2(a,b)              scanf("%d %d",&a, &b)
+#define sf3(a,b,c)            scanf("%d %d %d", &a, &b, &c)
 
-inline bool mo_cmp( const pair < ii, int > &x, const pair < ii, int > &y )
+#define sf1ll(a)              scanf("%lld", &a)
+#define sf2ll(a,b)            scanf("%lld %lld", &a, &b)
+#define sf3ll(a,b,c)          scanf("%lld %lld %lld", &a, &b, &c)
+
+template<class T1> void deb(T1 e1)
 {
-    int block_x = x.first.first / BLOCK_SIZE;
-    int block_y = y.first.first / BLOCK_SIZE;
-
-    if( block_x != block_y )
-    {
-        return block_x < block_y;
-    }
-    return x.first.second < y.first.second;
+    cout << e1 << endl;
+}
+template<class T1,class T2> void deb(T1 e1, T2 e2)
+{
+    cout << e1 << space << e2 << endl;
+}
+template<class T1,class T2,class T3> void deb(T1 e1, T2 e2, T3 e3)
+{
+    cout << e1 << space << e2 << space << e3 << endl;
 }
 
-inline void add( int x )
+#define ll long long
+const int res = 3e5 + 10;
+
+void reset();
+
+int a[1000005], tree[ res * 4 ], seen[ 1000005 ], ans[ 1000005 ];
+
+void build( int node, int b, int e )
 {
-    digit[ x ]++;
-    if( digit[x] == 1 )
+    if( b == e )
     {
-        current_answer++;
+        tree[node] = a[b];
+        return;
     }
+    int left = node * 2;
+    int right = left + 1;
+    int mid = ( b + e ) / 2;
+
+    build( left, b, mid );
+    build( right, mid + 1, e );
+    tree[node] = tree[left] + tree[right];
 }
 
-inline void rmv( int x )
+int query( int node, int b, int e, int i, int j )
 {
-    digit[ x ]--;
-    if( digit[x] == 0 )
+    if( i > e or j < b )
     {
-        current_answer--;
+        return 0;
     }
+    if( b >= i and e <= j )
+    {
+        return tree[node];
+    }
+    int left = node * 2;
+    int right = node * 2 + 1;
+    int mid = ( b + e ) / 2;
+
+    ll p1 = query( left, b, mid, i, j );
+    ll p2 = query( right, mid + 1, e, i, j );
+
+    return p1 + p2;
 }
+
+void update( int node, int b, int e, int i, int value )
+{
+    if( i > e or i < b )
+    {
+        return;
+    }
+    if( b >= i and e <= i )
+    {
+        tree[node] = value;
+        return;
+    }
+    int left = node * 2;
+    int right = node * 2 + 1;
+    int mid = ( b + e ) / 2;
+
+    update( left, b, mid, i, value );
+    update( right, mid + 1, e, i, value );
+    tree[ node ] = tree[left] + tree[right];
+}
+
 
 int main()
 {
-    scanf("%d", &n );
-    BLOCK_SIZE = static_cast <int> ( sqrt( n ) );
+    int test, n, q, sum, total, loop = 0;
 
-    for( int i = 0; i < n; i++ )
+    sf1( n );
+
+    for( int i = 1; i <= n; i++ )
     {
-        scanf("%d", &a[i] );
+        sf1( a[ i ] );
     }
+    //memset( seen, -1, sizeof seen );
+   // memset( tree, 0, sizeof tree );
+    vector < pair < int, pair < int, int > > >v;
 
-    scanf("%d", &q );
+    sf1( q );
 
     for( int i = 0; i < q; i++ )
     {
-        //cin >> queries[i].first.first >> queries[i].first.second;
-        scanf("%I64d %I64d", &queries[i].first.first, &queries[i].first.second );
-        queries[i].first.first--, queries[i].first.second--;
-        queries[i].second = i;
+        int l, r;
+        sf2( l, r );
+        v.push_back( make_pair( r, make_pair( l, i ) ) );
     }
 
-    sort( queries, queries + q, mo_cmp );
+    sort( v.begin(), v.end() );
+    int idx = 0, l, r, index;
 
-    int mo_left = 0, mo_right = -1;
+    for( int i = 1; i <= n; i++ )
+    {
+        l = a[ i ];
+        if( seen[ l ] == -1 )
+        {
+            seen[ l ] = i;
+            update( 1, 1, n, i, 1 );
+        }
+        else
+        {
+            r = seen[ l ];
+            update( 1, 1, n, r, 0 );
+            update( 1, 1, n, i, 1 );
+            seen[ l ] = i;
+        }
+            while( v[ idx ].first == i )
+            {
+                l = v[ idx ].second.first;
+                r = v[ idx ].first;
+                index = v[ idx ].second.second;
+                ans[ index ] = query( 1, 1, n, l, r );
+                idx++;
+            }
 
+    }
+    //printf("Case %d:\n", ++loop );
     for( int i = 0; i < q; i++ )
     {
-        int left = queries[i].first.first;
-        int right = queries[i].first.second;
-
-        while( mo_right < right )
-        {
-            mo_right++;
-            add( a[ mo_right ] );
-        }
-
-        while( mo_right > right )
-        {
-            rmv( a[ mo_right ] );
-            mo_right--;
-        }
-
-        while( mo_left < left )
-        {
-            rmv( a[ mo_left ] );
-            mo_left++;
-        }
-
-        while( mo_left > left )
-        {
-            mo_left--;
-            add( a[ mo_left ] );
-        }
-
-        answers[ queries[i].second ] = current_answer;
+        printf("%d\n", ans[ i ] );
     }
-
-    for( int i = 0; i < q; i++ )
-    {
-        printf("%I64d\n", answers[i] );
-    }
-
     return 0;
 }
+
+
+
+
+
+
